5438843220959defe5f5d055877f0ac0ae812766
Rodrigo-Palma <email.rodrigopalma@gmail.com>
2022-07-02 09:44:48 -0300

    created function for read files

--- public/main.js
+++ public/main.js
@@ -1,1 +1,17 @@
-console.log('test');
+function init() {
+    const input = document.getElementById('upload')
+    const fileReader = new FileReader()
+
+    fileReader.onloadend = () => {
+        let base64 = fileReader.result
+        console.log(input.files[0])
+        console.log(base64)
+    }
+
+
+    input.addEventListener('change', () => {
+        fileReader.readAsDataURL(input.files[0])
+    })
+}
+
+init()
